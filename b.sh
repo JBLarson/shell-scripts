da1dcd8bdb05b04685d622a3fecba74822c44ec8
JB Larson <JB@hirejb.me>
2024-08-16 22:43:54 -0400

    Update b.sh

--- b.sh
+++ b.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-echo && echo 'Homebrew Update / Upgrade / Cleanup / Doctor' && echo
+echo """ _          _     
+| |__   ___| |__  
+| '_ \ / __| '_ \ 
+| |_) |\__ \ | | |
+|_.__(_)___/_| |_|\n"""
+echo && echo """Homebrew Update / Upgrade / Cleanup / Doctor\n"""
+
 
 brew update && echo && brew upgrade && echo brew cleanup && echo && brew doctor
 echo
