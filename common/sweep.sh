aab6c3f5397ca40319c57260bd104b3db4ac80e7
JB Larson <JB@hirejb.me>
2025-12-12 12:31:20 -0800

    remove day from sweep dir string

--- common/sweep.sh
+++ common/sweep.sh
@@ -1,26 +1,30 @@
-#!/bin/sh
-# Move all PNG screenshots from Desktop to organized date-stamped folders
+#!/bin/bash
+# Sweep PNG screenshots on Desktop to /Documents/screenshots/{date}
 
+# Get current date components
 month=$(date +%m)
-day=$(date +%d)
 year=$(date +%y)
 
-folder_name="${month}_${day}_${year}"
+# Construct folder name
+folder_name="${month}_${year}"
 
+# Define paths
 desktop_path="$HOME/Desktop"
 screenshots_base="$HOME/Documents/screenshots"
 target_folder="${screenshots_base}/${folder_name}"
 
+# Create target folder if it doesn't exist
 mkdir -p "$target_folder"
 
+# Move all PNG files and count them
 moved_count=0
 while IFS= read -r -d '' file; do
-	mv "$file" "$target_folder/"
-	((moved_count++))
+    mv "$file" "$target_folder/"
+    ((moved_count++))
 done < <(find "$desktop_path" -maxdepth 1 -type f -iname "*.png" -print0)
 
 if [ "$moved_count" -eq 0 ]; then
-	echo "No PNG files found on Desktop"
+    echo "No PNG files found on Desktop"
 else
-	echo "Moved $moved_count PNG file(s) to $target_folder"
+    echo "Moved $moved_count PNG file(s) to $target_folder"
 fi
