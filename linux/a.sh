ef5564411cf542377fcd1123df7e5365171dd19a
JB Larson <68449234+JBLarson@users.noreply.github.com>
2026-01-17 14:19:18 -0800

    Update a.sh

--- linux/a.sh
+++ linux/a.sh
@@ -1,20 +1,16 @@
 #!/usr/bin/sh
-
-echo """             _     
+echo '             _
             | |    
   __ _   ___| |__  
- / _` | / __| '_ \ 
+ / _` | / __| '"'"'_ \ 
 | (_| |_\__ \ | | |
- \____(_)___/_| |_|\n"""
-
-
+ \____(_)___/_| |_|
+'
 echo
 echo "APT Update / Upgrade / Cleanup & Neofetch"
 echo
-
 echo "sudo apt-get update"      && sudo apt-get update      && echo
 echo "sudo apt-get upgrade -y"  && sudo apt-get upgrade -y  && echo
 echo "sudo apt-get autoremove -y" && sudo apt-get autoremove -y && echo
 echo "sudo apt-get autoclean"   && sudo apt-get autoclean   && echo
-
 neofetch
